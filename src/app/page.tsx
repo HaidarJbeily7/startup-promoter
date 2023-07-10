f6f882656be935265bd50db14c2d39b5741d58c8
Haidar Jbeily <h.jbeily@innopolis.university>
2023-07-10 14:55:09 +0300

    Build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
-import Image from "next/image";
+"use client"
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+
+  // const router = useRouter()
+  // router.push('/products');
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
     
